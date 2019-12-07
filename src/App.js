cf4248115e2c07d8f796cc3281521a514813c38b
Saulius Vikerta <svikerta@kayak.com>
2019-12-07 13:21:12 +0200

    adding VideoStream

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FeedbackList from './components/FeedbackList';
+import VideoStream from './components/VideoStream';
 
 import './App.css';
 
@@ -8,9 +9,13 @@ function App() {
       { id: '01', name: 'Jonas', datetime: 'YYYY-MM-DD', score: 1, text: 'komentaras kaip vertina Jonas'},
       { id: '02', name: 'Aldona', datetime: 'YYYY-MM-DD', score: -1, text: 'komentaras kaip vertina Aldona'},
     ];
+  const eventId = 'WUWz6xmSzbk';
   return (
       <div>
         <FeedbackList feedbackList={feedbackList}/>
+        <VideoStream
+         videoUrl={`https://www.youtube.com/embed/${eventId}?controls=0`}
+       />
       </div>
   );
 }
